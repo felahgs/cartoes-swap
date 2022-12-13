52e7db2f12036fa985409dc4b8468384a1632da3
Felipe Souza <fgsouza93@gmail.com>
2022-12-13 12:27:28 -0300

    Initial commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import "./Home.css";
+import Form from "../../components/Form";
+import Button from "../../components/Button";
+
+function Home() {
+  return (
+    <div className="Home">
+      Hello World!!
+      <Form/>
+      <Button/>
+    </div>
+  );
+}
+
+export default Home;
