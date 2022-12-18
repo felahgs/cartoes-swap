ee0e2e8be7c5e020b6220f99ae969400d72e94fb
Felipe Souza <fgsouza93@gmail.com>
2022-12-17 21:29:48 -0300

    configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,65 +1,46 @@
 /* eslint-disable no-useless-escape */
 module.exports = {
-  "env": {
-    "node": true,
-    "browser": true,
-    "es2021": true,
-    "jest": true
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+    jest: true,
   },
-  "extends": [
+  extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react/jsx-runtime"
+    "plugin:react/jsx-runtime",
   ],
-  "overrides": [
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  "plugins": [
-    "react",
-    "@typescript-eslint",
-    "eslint-plugin-import-helpers"
-  ],
-  "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "double"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ],
-    "object-curly-spacing": [ "warn", "always" ],
-    "array-bracket-spacing": [ "warn", "always" ], 
+  plugins: ["react", "@typescript-eslint", "eslint-plugin-import-helpers"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "object-curly-spacing": ["warn", "always"],
     "import-helpers/order-imports": [
       "warn",
-      { 
+      {
         newlinesBetween: "always",
         groups: [
-          [ "/^react$/", "/^react-dom/", "/^react-use$/" ],
-          [ "/^react-bootstrap/" ],
-          [ "module" ],
+          ["/^react$/", "/^react-dom/", "/^react-use$/"],
+          ["/^react-bootstrap/"],
+          ["module"],
           "/services/",
-          [ "/^pages/", "/^components/" ],
-          [ "/^utils/", ],
-          [ "parent", "sibling", "index" ],
-          [ "/^styles/", "/^types/" ]
+          ["/^pages/", "/^components/"],
+          ["/^utils/"],
+          ["parent", "sibling", "index"],
+          ["/^styles/", "/^types/"],
         ],
         alphabetize: { order: "asc", ignoreCase: true }, // configures imports in alphabetical (ascending) order, ignoring case
-
       },
     ],
-  }
+  },
 };
