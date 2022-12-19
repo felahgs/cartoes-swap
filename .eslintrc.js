cc1ba30cf91f66cf7064bdfbe55e60dce7dae66b
Felipe Souza <fgsouza93@gmail.com>
2022-12-19 11:03:24 -0300

    update pre-commit message; add 'format' script

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,11 @@ module.exports = {
     es2021: true,
     jest: true,
   },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
